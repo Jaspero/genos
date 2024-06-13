88d8ba50ced08ceb0f8c44fbba82ccf4053d4c2b
flauc <filip.lauc93@gmail.com>
2024-06-13 10:00:02 +0200

    fix(cms): properties import

--- cms/src/routes/dashboard/[module]/[collection]/[id]/+page.svelte
+++ cms/src/routes/dashboard/[module]/[collection]/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import CommonFormPage from '$lib/common/CommonFormPage.svelte';
   import type { CommonEditFormPageData, CommonNewFormPageData } from '$lib/common/common-form-page';
+  import Properties from '../../../pages/forms/Properties.svelte';
 
   export let data: {
     data: CommonNewFormPageData | CommonEditFormPageData;
--- cms/src/routes/dashboard/pages/forms/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import Properties from './Properties.svelte';
-</script>
-
-<slot />
