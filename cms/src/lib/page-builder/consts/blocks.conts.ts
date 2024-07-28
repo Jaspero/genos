a43fac92d99bb931b5cbf1b4de7cdfa8016922ff
flauc <filip.lauc93@gmail.com>
2024-07-28 17:04:44 +0200

    feat(cms): added ordered and unordered lists to page builder

--- cms/src/lib/page-builder/consts/blocks.conts.ts
+++ cms/src/lib/page-builder/consts/blocks.conts.ts
@@ -229,6 +229,22 @@ export const BLOCKS = (forms: PageBuilderForm[], popups?: Popup[]) => {
       content: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ipsum dolor sit</p>`
     },
     {
+      id: 'ordered-list',
+      label: 'Ordered List',
+      category: 'Content',
+      media: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" style="width: 100%; height: 48px"><path d="M120-80v-60h100v-30h-60v-60h60v-30H120v-60h120q17 0 28.5 11.5T280-280v40q0 17-11.5 28.5T240-200q17 0 28.5 11.5T280-160v40q0 17-11.5 28.5T240-80H120Zm0-280v-110q0-17 11.5-28.5T160-510h60v-30H120v-60h120q17 0 28.5 11.5T280-560v70q0 17-11.5 28.5T240-450h-60v30h100v60H120Zm60-280v-180h-60v-60h120v240h-60Zm180 440v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360Z"/></svg>`,
+      select: true,
+      content: `<ol style="padding-left:2rem"><li>List Item</li><li>List Item</li><li>List Item</li></ol>`
+    },
+    {
+      id: 'unordered-list',
+      label: 'Unordered List',
+      category: 'Content',
+      media: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" style="width: 100%; height: 48px"><path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z"/></svg>`,
+      select: true,
+      content: `<ul style="padding-left:2rem"><li>List Item</li><li>List Item</li><li>List Item</li></ul>`
+    },
+    {
       id: 'page-link',
       label: 'Page Link',
       media: `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style="width: 100%; height: 48px"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`,
--- cms/src/routes/dashboard/management/email-templates/[id]/render-mjml.ts
+++ cms/src/routes/dashboard/management/email-templates/[id]/render-mjml.ts
@@ -57,6 +57,24 @@ export function renderMjMl(
 		},
 		assetManager: {
 			custom: true
+		},
+		blockManager: {
+			blocks: [
+				{
+					id: 'ordered-list',
+					label: 'Ordered List',
+					media: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" style="width: 100%; height: 48px"><path d="M120-80v-60h100v-30h-60v-60h60v-30H120v-60h120q17 0 28.5 11.5T280-280v40q0 17-11.5 28.5T240-200q17 0 28.5 11.5T280-160v40q0 17-11.5 28.5T240-80H120Zm0-280v-110q0-17 11.5-28.5T160-510h60v-30H120v-60h120q17 0 28.5 11.5T280-560v70q0 17-11.5 28.5T240-450h-60v30h100v60H120Zm60-280v-180h-60v-60h120v240h-60Zm180 440v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360Z"/></svg>`,
+					select: true,
+					content: `<ol style="padding-left:2rem"><li>List Item</li><li>List Item</li><li>List Item</li></ol>`
+				},
+				{
+					id: 'unordered-list',
+					label: 'Unordered List',
+					media: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" style="width: 100%; height: 48px"><path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z"/></svg>`,
+					select: true,
+					content: `<ul style="padding-left:2rem"><li>List Item</li><li>List Item</li><li>List Item</li></ul>`
+				}
+			]
 		}
 	});
 
@@ -102,11 +120,11 @@ export function renderMjMl(
 	);
 
 	grapesInstance.runCommand('core:component-outline');
-  grapesInstance.DomComponents.getWrapper()!.set({badgable: false, selectable: false});
+	grapesInstance.DomComponents.getWrapper()!.set({badgable: false, selectable: false});
 
 	if (json) {
-    grapesInstance.loadProjectData(json);
-  }
+		grapesInstance.loadProjectData(json);
+	}
 
-  return grapesInstance;
+	return grapesInstance;
 }
