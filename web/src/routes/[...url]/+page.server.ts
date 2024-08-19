9afc34bbfbb966a1b67c4a1f96177f068bba4d39
flauc <filip.lauc93@gmail.com>
2024-08-19 13:37:23 +0200

    fix(web): loading pages

--- web/src/routes/[...url]/+page.server.ts
+++ web/src/routes/[...url]/+page.server.ts
@@ -1,11 +1,10 @@
-import { storage } from '$lib/utils/firebase';
-import { firestore, bucket } from '$lib/utils/firebase-admin';
-import type { PageServerLoad } from './$types';
+import {firestore, bucket} from '$lib/utils/firebase-admin';
+import type {PageServerLoad} from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({params}) => {
   const url = params.url.trim();
 
-  const { docs } = await firestore
+  const {docs} = await firestore
     .collection('pages')
     .where('url', '==', '/' + url)
     .where('active', '==', true)
@@ -19,40 +18,50 @@ export const load: PageServerLoad = async ({ params }) => {
   }
 
   const toLoad: any[] = [
-     bucket.file(`page-configurations/pages/${doc.id}/content.html`).download(),
-     bucket.file(`page-configurations/pages/${doc.id}/content.css`).download()
+    bucket.file(`page-configurations/pages/${doc.id}/content.html`).download(),
+    bucket.file(`page-configurations/pages/${doc.id}/content.css`).download()
   ];
 
   const data = doc.data();
 
   if (data.header) {
     toLoad.push(
-       bucket.file(`page-configurations/layouts/${data.header}/content.html`).download(),
-       bucket.file(`page-configurations/layouts/${data.header}/content.css`).download()
+      bucket.file(`page-configurations/layouts/${data.header}/content.html`).download(),
+      bucket.file(`page-configurations/layouts/${data.header}/content.css`).download()
     );
   }
 
   if (data.footer) {
     toLoad.push(
-       bucket.file(`page-configurations/layouts/${data.footer}/content.html`).download(),
-       bucket.file(`page-configurations/layouts/${data.footer}/content.css`).download()
+      bucket.file(`page-configurations/layouts/${data.footer}/content.html`).download(),
+      bucket.file(`page-configurations/layouts/${data.footer}/content.css`).download()
     );
   }
 
   const [htmlRef, styleRef, ...layoutRefs] = await Promise.all(toLoad);
 
-  const content = [htmlRef, layoutRefs[0], layoutRefs[2]].reduce((acc, cur) => {
-    if (cur) {
-      acc += cur.toString();
-    }
-    return acc;
-  }, '');
-  const style = [styleRef, layoutRefs[1], layoutRefs[3]].reduce((acc, cur) => {
-    if (cur) {
-      acc += cur.toString();
-    }
-    return acc;
-  }, '');
+  const content = [
+    layoutRefs[0],
+    htmlRef,
+    layoutRefs[2]
+  ]
+    .reduce((acc, cur) => {
+      if (cur) {
+        acc += cur.toString();
+      }
+      return acc;
+    }, '');
+  const style = [
+    layoutRefs[1],
+    styleRef,
+    layoutRefs[3]
+  ]
+    .reduce((acc, cur) => {
+      if (cur) {
+        acc += cur.toString();
+      }
+      return acc;
+    }, '');
 
   return {
     ...data,
