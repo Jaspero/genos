9f7c64f84c65c31125962c602bce3c242d254d07
flauc <filip.lauc93@gmail.com>
2024-11-07 10:02:45 +0100

    fix(web): missing blog page

---
+++ web/src/routes/posts/[id]/+page.server.ts
@@ -0,0 +1,51 @@
+import { blogData } from '$lib/server/blog';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { CONFIG } from '$lib/consts/config.const';
+import { bucket, firestore } from '$lib/utils/firebase-admin';
+import { ENV_CONFIG } from '$lib/consts/env-config';
+
+export const load: PageServerLoad = async ({ params }) => {
+  const { articles } = await blogData();
+
+	const article = articles.find(it => it.url === params.id);
+
+	if (!article) {
+		return redirect(303, '/404');
+	}
+
+  const data = {
+    header: '',
+    footer: '',
+    article
+  };
+	const toLoad = [];
+
+	if (CONFIG.singleHeader) {
+    toLoad.push(
+      Promise.all([
+        bucket.file(`page-configurations/layouts/${CONFIG.singleHeader}/content.html`).download(),
+        bucket.file(`page-configurations/layouts/${CONFIG.singleHeader}/content.css`).download()
+      ])
+        .then(refs => {
+          data.header = `${refs?.[0].toString()}<style>${refs?.[1]?.toString()}</style>`;
+        })
+    );
+  }
+
+  if (CONFIG.singleFooter) {
+    toLoad.push(
+      Promise.all([
+        bucket.file(`page-configurations/layouts/${CONFIG.singleFooter}/content.html`).download(),
+        bucket.file(`page-configurations/layouts/${CONFIG.singleFooter}/content.css`).download()
+      ])
+        .then(refs => {
+          data.footer = `${refs?.[0].toString()}<style>${refs?.[1]?.toString()}</style>`;
+        })
+    );
+  }
+
+  await Promise.all(toLoad);
+
+  return data;
+};
