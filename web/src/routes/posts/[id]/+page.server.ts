b5cf1e50f1c16e95a7ffd1ea8c2582dece5ebd4c
Matej Maloča <maloca1992@gmail.com>
2024-11-07 10:23:03 +0100

    fix: unused import breaks build

--- web/src/routes/posts/[id]/+page.server.ts
+++ web/src/routes/posts/[id]/+page.server.ts
@@ -2,34 +2,32 @@ import { blogData } from '$lib/server/blog';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { CONFIG } from '$lib/consts/config.const';
-import { bucket, firestore } from '$lib/utils/firebase-admin';
-import { ENV_CONFIG } from '$lib/consts/env-config';
+import { bucket } from '$lib/utils/firebase-admin';
 
 export const load: PageServerLoad = async ({ params }) => {
   const { articles } = await blogData();
 
-	const article = articles.find(it => it.url === params.id);
+  const article = articles.find((it) => it.url === params.id);
 
-	if (!article) {
-		return redirect(303, '/404');
-	}
+  if (!article) {
+    return redirect(303, '/404');
+  }
 
   const data = {
     header: '',
     footer: '',
     article
   };
-	const toLoad = [];
+  const toLoad = [];
 
-	if (CONFIG.singleHeader) {
+  if (CONFIG.singleHeader) {
     toLoad.push(
       Promise.all([
         bucket.file(`page-configurations/layouts/${CONFIG.singleHeader}/content.html`).download(),
         bucket.file(`page-configurations/layouts/${CONFIG.singleHeader}/content.css`).download()
-      ])
-        .then(refs => {
-          data.header = `${refs?.[0].toString()}<style>${refs?.[1]?.toString()}</style>`;
-        })
+      ]).then((refs) => {
+        data.header = `${refs?.[0].toString()}<style>${refs?.[1]?.toString()}</style>`;
+      })
     );
   }
 
@@ -38,10 +36,9 @@ export const load: PageServerLoad = async ({ params }) => {
       Promise.all([
         bucket.file(`page-configurations/layouts/${CONFIG.singleFooter}/content.html`).download(),
         bucket.file(`page-configurations/layouts/${CONFIG.singleFooter}/content.css`).download()
-      ])
-        .then(refs => {
-          data.footer = `${refs?.[0].toString()}<style>${refs?.[1]?.toString()}</style>`;
-        })
+      ]).then((refs) => {
+        data.footer = `${refs?.[0].toString()}<style>${refs?.[1]?.toString()}</style>`;
+      })
     );
   }
 
