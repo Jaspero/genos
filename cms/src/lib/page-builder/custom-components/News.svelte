8f1d78deb5cd3e8612f1bcf7421d269c878e443f
kristian <kristiantarnaj@gmail.com>
2025-03-28 12:54:52 +0100

    feat(web): fix news

--- cms/src/lib/page-builder/custom-components/News.svelte
+++ cms/src/lib/page-builder/custom-components/News.svelte
@@ -64,11 +64,11 @@
           category ? where('category', '==', category) : null,
           author ? where('author', '==', author) : null
         ].filter(Boolean) as QueryConstraint[]),
-    where('active', '==', true),
-      orderBy('publicationDate', 'desc'),
-      limit(pageSize + 1)
-  )
-  );
+        where('active', '==', true),
+        orderBy('publicationDate', 'desc'),
+        limit(pageSize + 1)
+      )
+    );
 
     if (docs.length === pageSize + 1) {
       lastRef = docs[docs.length - 1];
@@ -89,12 +89,12 @@
           category ? where('category', '==', category) : null,
           author ? where('author', '==', author) : null
         ].filter(Boolean) as QueryConstraint[]),
-    where('active', '==', true),
-      orderBy('publicationDate', 'desc'),
-      startAt(lastRef),
-      limit(pageSize + 1)
-  )
-  );
+        where('active', '==', true),
+        orderBy('publicationDate', 'desc'),
+        startAt(lastRef),
+        limit(pageSize + 1)
+      )
+    );
 
     if (docs.length === pageSize + 1) {
       lastRef = docs[docs.length - 1];
@@ -124,75 +124,60 @@
   });
 </script>
 
-<svelte:head>
-  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-</svelte:head>
-
 <div class="grid grid-large services-grid">
-  <div class="col-span-4 tab-selectors">
-    <button class="tab-selector" class:active={tab === 1} on:click={() => tab = 1}>Our posts</button>
-    <button class="tab-selector" class:active={tab === 2} on:click={() => tab = 2}>Twitter feed</button>
-  </div>
-  <div class="col-span-8 tabs">
-    {#if tab === 2}
-      <a class="twitter-timeline" href="https://twitter.com/GGlycoscience?ref_src=twsrc%5Etfw">Tweets by GGlycoscience</a>
-    {:else if tab === 1}
-      {#if showCategoryFilters}
-        <div class="container categories-container">
-          <button
-            class="cat-btn"
-            type="button"
-            class:active={category === ''}
-            on:click={() => {
+  <div class="col-span-3 tab-selectors">
+    {#if showCategoryFilters}
+      <button
+        class="tab-selector"
+        type="button"
+        class:active={category === ''}
+        on:click={() => {
         category = '';
         typeChanges(pageSize, category, author, authorMap);
       }}>{allcategorieslabel}</button
-          >
-          {#each categories as cat}
-            <button
-              type="button"
-              class="cat-btn"
-              class:active={category === cat.id}
-              on:click={() => {
-          category = cat.id;
-          typeChanges(pageSize, category, author, authorMap);
-        }}>{cat.name}</button
-            >
-          {/each}
-        </div>
-      {/if}
-
-      {#if articles}
-        <div class="container blog-container">
-          {#each articles as article}
-            <a class="post-card" href={'/posts/' + article.url}>
-              <img src={article.image} alt={article.imageAlt || article.title} class="news-img" />
-              <div class="post-content-wrapper">
-                {#if article.category}
-                  <div class="post-category-wrapper">
-                    <span class="news-category">{categoryMap[article.category]?.name || ''}</span>
-                  </div>
-                {/if}
-                <div class="post-content">
-                  <h3 class="h3">{article.title}</h3>
-                  {#if article.publicationDate}
-                    <p class="post-date">
-                      {datelabel}: {new Date(article.publicationDate).toLocaleDateString()}
-                    </p>
-                  {/if}
-                  <p class="p">{article.description}</p>
-                </div>
+      >
+      {#each categories as cat}
+        <button class="tab-selector"
+                class:active={category === cat.id}
+                on:click={() => {
+                category = cat.id;
+                typeChanges(pageSize, category, author, authorMap);
+              }}>
+          {cat.name}
+        </button>
+      {/each}
+    {/if}
+  </div>
+  <div class="col-span-9">
+    {#if articles.length}
+      <div class="grid">
+        {#each articles as article}
+          <a class="article col-span-6" href={'/posts/' + article.url}>
+            <div class="article-image">
+              <img src={article.image} alt={article.imageAlt || article.title} />
+              {#if article.category}
+                <div class="article-category">{categoryMap[article.category]?.name || ''}</div>
+              {/if}
+              <div class="article-content">
+                <h3 class="article-title">{article.title}</h3>
+                <p class="article-description">{article.description}</p>
               </div>
-            </a>
-          {/each}
+            </div>
+          </a>
+        {/each}
+      </div>
+    {:else}
+      <div class="grid">
+        <div class="col-span-12">
+          No posts yet
         </div>
-      {/if}
+      </div>
+    {/if}
 
-      {#if lastRef}
-        <button type="button" class:loading={loading || resetLoading} on:click={loadMore}
-        >{loadmorelabel}</button
-        >
-      {/if}
+    {#if lastRef}
+      <button type="button" class:loading={loading || resetLoading} on:click={loadMore}
+      >{loadmorelabel}</button
+      >
     {/if}
   </div>
 </div>
